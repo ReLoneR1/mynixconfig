bb3716095c1b75b8d6aaed41e42ed80ad13fc034
Oleg Kaminskiy <157854055+ReLoneR1@users.noreply.github.com>
2024-10-17 22:07:20 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
